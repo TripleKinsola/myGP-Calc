3c1cc7f7649a18d178b886aa8b4bfbe72c6037a8
geekERbyte <triplekinsola@gmail.com>
2015-04-09 15:06:09 +0100

    manipulate index.php for quick style fetching

--- assets/includes/header.inc.php
+++ assets/includes/header.inc.php
@@ -9,40 +9,4 @@
 */
 ini_set('display_errors', false);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="./assets/img/favicon.ico">
-    <title>Welcome | CGPA-Calc</title>
-    <link href="./dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./dist/css/sticky-footer.css" rel="stylesheet">
 
-    <script src="./dist/js/respond.min.js"></script>
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span style="color:#fff;">
-            <span class="glyphicon glyphicon-user"></span>
-            MENU</span>
-          </button>
-          <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home" ></span> myCGPA-Calc</a>
-        </div>
-        <div class="collapse navbar-collapse pull-right" style="color:#abcedf; font-size: 27px; padding-top:10px;">
-          <span class="dropdown-toggle" data-toggle="dropdown">
-            <i class="glyphicon glyphicon-user"></i> Created by: <a href="http://geekerbyte.blogspot.com" target="_blank">Akinsola Ademola</a>
-          </span>
-        </div>
-
-      </div>
-    </div>
